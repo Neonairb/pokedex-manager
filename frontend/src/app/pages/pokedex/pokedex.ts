86cc14e51b5304eef67fafbcfd543ab2a5882cbe
NeoNairb <brajai@hotmail.com>
2026-08-14 14:49:36 -0600

    add basic page and auth structure

---
+++ frontend/src/app/pages/pokedex/pokedex.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokedex',
+  imports: [],
+  templateUrl: './pokedex.html',
+  styleUrl: './pokedex.css',
+})
+export class Pokedex {}
