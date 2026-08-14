c936c1589a5a8ffe719759fb070dfd960b8df5d5
NeoNairb <brajai@hotmail.com>
2026-08-14 17:17:00 -0600

    add routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,49 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import(
+        './pages/auth/login/login'
+      ).then((m) => m.Login),
+  },
+
+  {
+    path: 'register',
+    loadComponent: () =>
+      import(
+        './pages/auth/register/register'
+      ).then((m) => m.Register),
+  },
+
+  {
+    path: 'home',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import(
+        './pages/home/home'
+      ).then((m) => m.Home),
+  },
+
+  {
+    path: 'pokedex',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import(
+        './pages/pokedex/pokedex'
+      ).then((m) => m.Pokedex),
+  },
+
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
