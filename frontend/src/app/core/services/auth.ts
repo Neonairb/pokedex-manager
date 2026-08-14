0ae5bfdda1efd5505f69c8f7059efe7256666763
NeoNairb <brajai@hotmail.com>
2026-08-14 15:09:39 -0600

    add auth

--- frontend/src/app/core/services/auth.ts
+++ frontend/src/app/core/services/auth.ts
@@ -1,4 +1,62 @@
-import { Service } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Service } from '@angular/core';
+import { tap } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+
+import {
+  AuthResponse,
+  LoginRequest,
+  RegisterRequest,
+} from '../models/auth.model';
 
 @Service()
-export class Auth {}
+export class Auth {
+  private readonly http = inject(HttpClient);
+
+  private readonly apiUrl = `${environment.apiUrl}/auth`;
+
+  private readonly tokenKey = 'accessToken';
+
+  register(data: RegisterRequest) {
+    return this.http
+      .post<AuthResponse>(
+        `${this.apiUrl}/register`,
+        data,
+      )
+      .pipe(
+        tap((response) => {
+          this.saveToken(response.accessToken);
+        }),
+      );
+  }
+
+  login(data: LoginRequest) {
+    return this.http
+      .post<AuthResponse>(
+        `${this.apiUrl}/login`,
+        data,
+      )
+      .pipe(
+        tap((response) => {
+          this.saveToken(response.accessToken);
+        }),
+      );
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.tokenKey);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  isAuthenticated(): boolean {
+    return this.getToken() !== null;
+  }
+
+  private saveToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+}
