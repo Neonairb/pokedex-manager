9fdb589cdf43cbdbb115168d854f14c5f54e2397
NeoNairb <brajai@hotmail.com>
2026-08-13 23:15:58 -0600

    add markAsSeen

--- backend/src/pokedex/pokedex.module.ts
+++ backend/src/pokedex/pokedex.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PokedexController } from './pokedex.controller';
 import { PokedexService } from './pokedex.service';
+import { PokemonModule } from '../pokemon/pokemon.module';
 
 @Module({
+  imports: [PokemonModule],
   controllers: [PokedexController],
-  providers: [PokedexService]
+  providers: [PokedexService],
+  exports: [PokedexService],
 })
 export class PokedexModule {}
--- backend/src/pokedex/pokedex.service.ts
+++ backend/src/pokedex/pokedex.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { PokemonService } from '../pokemon/pokemon.service';
+import { EncounterDto } from './dto/encounter.dto';
+import { ScanPokemonDto } from './dto/scan-pokemon.dto';
 
 @Injectable()
-export class PokedexService {}
+export class PokedexService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly pokemonService: PokemonService,
+  ) {}
+
+  private async markAsSeen(
+    userId: number,
+    pokemonId: number,
+  ) {
+    const existing =
+      await this.prisma.userPokemon.findUnique({
+        where: {
+          userId_pokemonId: {
+            userId,
+            pokemonId,
+          },
+        },
+      });
+
+    if (existing?.status === 'SCANNED') {
+      return existing;
+    }
+
+    return this.prisma.userPokemon.upsert({
+      where: {
+        userId_pokemonId: {
+          userId,
+          pokemonId,
+        },
+      },
+
+      create: {
+        userId,
+        pokemonId,
+        status: 'SEEN',
+      },
+
+      update: {
+        status: 'SEEN',
+      },
+    });
+  }
+}
