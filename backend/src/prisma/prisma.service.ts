98a7f87ebff332aae6ff1fd0981c7b0371dd3704
NeoNairb <brajai@hotmail.com>
2026-08-13 19:47:15 -0600

    fix align generated client with CommonJS build

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client.cjs';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(configService: ConfigService) {
+    const connectionString =
+      configService.getOrThrow<string>('DATABASE_URL');
+
+    const adapter = new PrismaPg({
+      connectionString,
+    });
+
+    super({ adapter });
+  }
+}
